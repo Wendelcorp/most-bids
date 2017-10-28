21a2a1e49748c5bd6146a3c2454d32d684bc2738
Wendelcorp <bryce@brycewendelaar.com>
2017-10-28 15:31:57 -0400

    Added image url

--- bidfinder.rb
+++ bidfinder.rb
@@ -5,11 +5,9 @@ puts "Search for an item to see popular results"
 term = gets.chomp
 link = "https://www.ebay.com/sch/" + term.gsub!(/ /, "+") + "?isRefine=true&LH_Auction=1&_ipg=200"
 page = Nokogiri::HTML(open(link))
-
-@items = []
-
 number_of_pages = ((page.css('.rcnt').text.to_f / 200)).ceil
 
+@items = []
 i = number_of_pages
 while i > 0
   new_link = link + "&_pgn=#{i}"
@@ -19,7 +17,7 @@ while i > 0
     bids = item.css('.lvformat span').text.split.join(" ").gsub(/bids/, 'bid' => '', 's' => '').to_i
     item_hash['bids'] = bids
     item_hash['name'] = item.css('h3 a').text.split.join(" ")
-    # item_hash['item_number'] = item.css('h3 a').text.split.join(" ")
+    # item_hash['img'] = item.css('.lvpicinner img')[0]['src']
     if bids > 0
       @items << item_hash
     end
